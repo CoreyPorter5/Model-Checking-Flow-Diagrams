aef6d0a4d9c0e154f9e22f99b0c18a59d4d42af0
CoreyPorter5 <132121576+CoreyPorter5@users.noreply.github.com>
2026-05-06 21:19:13 +0200

    Basic product classes created

--- src/main/java/ProductCFG.java
+++ src/main/java/ProductCFG.java
@@ -1,2 +1,57 @@
+import java.util.*;
+
 public class ProductCFG {
+    private String startSymbol;
+    private Set<String> variables;
+    private Set<String> terminals;
+    private List<Production> productions;
+
+    public ProductCFG() {
+        this.startSymbol = "S";
+        this.variables = new HashSet<>();
+        this.terminals = new HashSet<>();
+        this.productions = new ArrayList<>();
+        this.variables.add(startSymbol);
+    }
+
+    public void addProduction(String left, List<String> right) {
+        variables.add(left);
+        productions.add(new Production(left, right));
+
+        for (String symbol : right) {
+            if (isVariable(symbol)) {
+                variables.add(symbol);
+            } else if (!symbol.equals("eps")) {
+                terminals.add(symbol);
+            }
+        }
+    }
+
+    private boolean isVariable(String symbol) {
+        return symbol.equals("S") || symbol.startsWith("[");
+    }
+
+    public String getStartSymbol() {
+        return startSymbol;
+    }
+
+    public Set<String> getVariables() {
+        return variables;
+    }
+
+    public Set<String> getTerminals() {
+        return terminals;
+    }
+
+    public List<Production> getProductions() {
+        return productions;
+    }
+
+    public void print() {
+        System.out.println("-----------PRODUCT CFG-----------");
+
+        for (Production p : productions) {
+            System.out.println(p);
+        }
+    }
 }
--- src/main/java/Production.java
+++ src/main/java/Production.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
 public class Production {
+    private String left;
+    private List<String> right;
+
+    public Production(String left, List<String> right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public String getLeft(){
+        return left;
+    }
+
+    public List<String> getRight(){
+        return right;
+    }
+
+    @Override
+    public String toString(){
+        if(right.isEmpty()){
+            return left + "-> eps";
+        }
+        return left + " -> " + String.join(" ", right);
+    }
 }
